d5da42b9014a212f8bff54dcbcca1394b6aa127f
Daniel Escalona <daniel07escalona@gmail.com>
2022-01-10 12:30:01 -0500

    commit tree 6e6598f2fdecb4f5625d30b956edf998876c9b80

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Daniel";
+let welcomeMessage = "Welcome to Flatbook, ";
+welcomeMessage = welcomeMessage+currentUser+'!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
